3a2af19772eb563df9d7b8bb3dc528497816269e
Florian Vandecasteele <47397500+Flotroniks@users.noreply.github.com>
2023-05-09 20:49:57 +0200

    c

---
+++ src/atomes/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Button(){
+    return (
+        <div className="bg-black text-white py-2 px-4 w-80 flex justify-center rounded transition hover:text-black hover:bg-white duration-300">
+            Ajouter au panier
+        </div>
+
+    );
+}
